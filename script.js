05b3a11c8d827af084ff43792e6eca1d534b9d22
MOON-PR <raif.moon2000@gmail.com>
2024-09-18 03:27:33 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+"use strict";
+var _a;
+// listing element
+(_a = document.getElementById('resumeForm')) === null || _a === void 0 ? void 0 : _a.addEventListener('submit', function (event) {
+    event.preventDefault();
+    //type assertion
+    const nameElement = document.getElementById('name');
+    const emailElement = document.getElementById('email');
+    const phoneElement = document.getElementById('phone');
+    const educationElement = document.getElementById('education');
+    const expirienceElement = document.getElementById('expirience');
+    const skillsElement = document.getElementById('skills');
+    if (nameElement && emailElement && phoneElement && educationElement && expirienceElement && skillsElement)
+        //create resume output
+        const resumeOutput = `
+    <h2>Resume</h2> 
+    <p><strong>Name:</strong> ${name} </p>
+`;
+});
